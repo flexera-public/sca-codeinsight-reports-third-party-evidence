7f63c9bcce7603f1d1b2555eebd2bc599c19e7f2
Sean Geary <sgeary@flexera.com>
2021-03-24 17:59:47 -0400

    Add summary details

--- report_artifacts.py
+++ report_artifacts.py
@@ -38,6 +38,7 @@ def generate_html_report(reportData):
     reportName = reportData["reportName"]
     projectName = reportData["projectName"] 
     fileEvidence = reportData["fileEvidence"]
+    evidenceSummary = reportData["evidenceSummary"]
    
     scriptDirectory = os.path.dirname(os.path.realpath(__file__))
     cssFile =  os.path.join(scriptDirectory, "html-assets/css/revenera_common.css")
@@ -105,7 +106,7 @@ def generate_html_report(reportData):
     html_ptr.write("        </style>\n")  
 
     html_ptr.write("    	<link rel='icon' type='image/png' href='data:image/png;base64, {}'>\n".format(encodedfaviconImage.decode('utf-8')))
-    html_ptr.write("        <title>%s</title>\n" %(reportName.upper()))
+    html_ptr.write("        <title>%s</title>\n" %(reportName))
     html_ptr.write("    </head>\n") 
 
     html_ptr.write("<body>\n")
@@ -119,7 +120,7 @@ def generate_html_report(reportData):
     html_ptr.write("  <div class='logo'>\n")
     html_ptr.write("    <img src='data:image/svg+xml;base64,{}' style='width: 400px;'>\n".format(encodedLogoImage.decode('utf-8')))
     html_ptr.write("  </div>\n")
-    html_ptr.write("<div class='report-title'>%s</div>\n" %reportName)
+    html_ptr.write("<div class='report-title'>%s</div>\n" %(reportName))
     html_ptr.write("</div>\n")
     html_ptr.write("<!-- END HEADER -->\n")
 
@@ -128,6 +129,20 @@ def generate_html_report(reportData):
     #---------------------------------------------------------------------------------------------------
     html_ptr.write("<!-- BEGIN BODY -->\n") 
  
+    html_ptr.write("<table id='evidenceSummary' class='table' style='width:90%'>\n")
+    html_ptr.write("    <thead>\n")
+    html_ptr.write("        <tr>\n") 
+    html_ptr.write("            <th class='text-center'><h4>%s - Third Party Evidence Summary</h4></th>\n"%projectName) 
+    html_ptr.write("        </tr>\n") 
+    html_ptr.write("    </thead>\n")
+    html_ptr.write("    <tbody>\n")
+    html_ptr.write("        <tr><td>\n") 
+    html_ptr.write("            <div style=\"height: 300px\" class='container'>\n")
+    html_ptr.write("                <canvas id='projectEvidence'></canvas>\n")
+    html_ptr.write("            </div>\n")
+    html_ptr.write("        </td></tr>\n") 
+    html_ptr.write("    </body>\n")
+    html_ptr.write("</table>\n")
 
     html_ptr.write("<hr class='small'>")
 
@@ -135,16 +150,16 @@ def generate_html_report(reportData):
 
     html_ptr.write("    <thead>\n")
     html_ptr.write("        <tr>\n")
-    html_ptr.write("            <th colspan='10' class='text-center'><h4>Files with Third Party Evidence</h4></th>\n") 
+    html_ptr.write("            <th colspan='10' class='text-center'><h4>%s - Third Party Evidence File Details</h4></th>\n" %projectName) 
     html_ptr.write("        </tr>\n") 
     html_ptr.write("        <tr>\n")
-    html_ptr.write("            <th style='width: 50%' class='text-left text-nowrap'>File Path</th>\n") 
-    html_ptr.write("            <th style='width: 5%' class='text-center'>Copyright</th>\n") 
-    html_ptr.write("            <th style='width: 5%' class='text-center'>License</th>\n")
-    html_ptr.write("            <th style='width: 5%' class='text-center'>Email/URL</th>\n")
-    html_ptr.write("            <th style='width: 5%' class='text-center'>Search Terms</th>\n")
-    html_ptr.write("            <th style='width: 5%' class='text-center'>Exact File Match</th>\n")
-    html_ptr.write("            <th style='width: 5%' class='text-center'>Source Code Match</th>\n")
+    html_ptr.write("            <th style='vertical-align: middle; width: 50%' class='text-left text-nowrap'>File Path</th>\n") 
+    html_ptr.write("            <th style='vertical-align: middle; width: 5%' class='text-center'>Copyright</th>\n") 
+    html_ptr.write("            <th style='vertical-align: middle; width: 5%' class='text-center'>License</th>\n")
+    html_ptr.write("            <th style='vertical-align: middle; width: 5%' class='text-center'>Email/URL</th>\n")
+    html_ptr.write("            <th style='vertical-align: middle; width: 5%' class='text-center'>Search Terms</th>\n")
+    html_ptr.write("            <th style='vertical-align: middle; width: 5%' class='text-center'>Exact File Match</th>\n")
+    html_ptr.write("            <th style='vertical-align: middle; width: 5%' class='text-center'>Source Code Match</th>\n")
     html_ptr.write("        </tr>\n")
     html_ptr.write("    </thead>\n")  
     html_ptr.write("    <tbody>\n")  
@@ -159,7 +174,7 @@ def generate_html_report(reportData):
         for evidence in ["copyright", "license", "emailURL", "searchTerm", "exactMatch", "sourceMatch"]:
             # See if the evidence exists.  If the key is not there that type was not found
             if evidence in fileEvidence[filePath]["evidence"]:
-                html_ptr.write("            <td class='text-center text-nowrap'><span class='dot dot-%s'></span></td>\n" %evidence)
+                html_ptr.write("            <td class='text-center text-nowrap' style='vertical-align: middle;'><span class='dot dot-%s'></span></td>\n" %evidence)
             else:
                 html_ptr.write("            <td>&nbsp</td>\n")
 
@@ -192,10 +207,17 @@ def generate_html_report(reportData):
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>  
-    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script> 
+    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script> 
     ''')
 
     html_ptr.write("<script>\n")
+
+     # Add the common chartjs config
+    add_default_chart_options(html_ptr)
+    # Add the js for the evidence summary stacked bar charts
+    generate_evidence_summary_chart(html_ptr, evidenceSummary)
+
     # Add the js for inventory datatable
     html_ptr.write('''
         
@@ -228,3 +250,98 @@ def encodeImage(imageFile):
     except:
         logger.error("Unable to open %s" %imageFile)
         raise
+
+#----------------------------------------------------#
+def add_default_chart_options(html_ptr):
+    # Add commont defaults for display charts
+    html_ptr.write('''  
+        var defaultBarChartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        layout: {
+            padding: {
+                bottom: 25  //set that fits the best
+            }
+        },
+        tooltips: {
+            enabled: false,
+            yAlign: 'center'
+        },
+        title: {
+            display: false,
+            fontColor: "#323E48"
+        },
+
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    fontSize:12,
+                    fontColor: "#323E48",
+                    precision:0
+
+                },
+                scaleLabel:{
+                    display:false
+                },
+                gridLines: {
+                }, 
+                stacked: false
+            }],
+            yAxes: [{
+                gridLines: {
+                    display:false,
+                    color: "#fff",
+                    zeroLineColor: "#fff",
+                    zeroLineWidth: 0,
+                    fontColor: "#323E48"
+                },
+                ticks: {
+                    fontSize:15,
+                    fontColor: "#323E48"
+                },
+
+                stacked: false
+            }]
+        },
+        legend:{
+            display:false
+        },
+        
+    };  ''')
+
+#----------------------------------------------------#
+def generate_evidence_summary_chart(html_ptr, evidenceSummary):
+    logger.info("Entering generate_evidence_summary_chart")
+    html_ptr.write('''  
+        var projectEvidence = document.getElementById("projectEvidence");
+        var projectEvidenceChart = new Chart(projectEvidence, {
+            type: 'horizontalBar',
+            data: {
+                labels: ['Total Files Scanned (%s files)', 'Copyleft (%s files - %s%%)', 'License (%s files - %s%%)','Email/URL (%s files - %s%%)','Search Terms (%s files - %s%%)','Exact Match (%s files - %s%%)','Source Match (%s files - %s%%)'],
+                datasets: [{
+                    data: [%s, %s, %s, %s, %s, %s, %s],
+                    backgroundColor: ["#7F7F7F", "#0099FF", "#33CC33", "#FF02F9", "#02FFFF", "#FF0000", "#FFFF00"]
+                }]
+            },
+
+            options: defaultBarChartOptions,
+        });
+
+
+        ''' %(evidenceSummary["totalScannedFiles"],
+            evidenceSummary["copyright"], str(int(evidenceSummary["copyright"]/evidenceSummary["totalScannedFiles"]*100)),
+            evidenceSummary["license"], str(int(evidenceSummary["license"]/evidenceSummary["totalScannedFiles"]*100)),
+            evidenceSummary["emailURL"], str(int(evidenceSummary["emailURL"]/evidenceSummary["totalScannedFiles"]*100)),
+            evidenceSummary["searchTerm"], str(int(evidenceSummary["searchTerm"]/evidenceSummary["totalScannedFiles"]*100)),
+            evidenceSummary["exactMatch"], str(int(evidenceSummary["exactMatch"]/evidenceSummary["totalScannedFiles"]*100)),
+            evidenceSummary["sourceMatch"], str(int(evidenceSummary["sourceMatch"]/evidenceSummary["totalScannedFiles"]*100)),
+            evidenceSummary["totalScannedFiles"],
+            evidenceSummary["copyright"], 
+            evidenceSummary["license"], 
+            evidenceSummary["emailURL"],
+            evidenceSummary["searchTerm"],
+            evidenceSummary["exactMatch"],
+            evidenceSummary["sourceMatch"]         
+            
+            )  )
